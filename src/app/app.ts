616d0849b3efe8e3c9e4498d7707f3f698d553cc
YazykovaDaria <yazykovadasva@gmail.com>
2023-10-27 22:38:54 +0300

    refactor: structure

---
+++ src/app/app.ts
@@ -0,0 +1,9 @@
+import form from '../components/form/form';
+import modal from '../components/modal/modal';
+
+const app = () => {
+  modal();
+  form();
+};
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
 import './index.html';
 import 'reset-css';
 import './global.scss';
+import app from './app/app';
 
-const y = (n, d) => n + d;
+app();
 
-console.log(y(3, 5));
+// import MaskInput from 'mask-input';
+
+
+// // import { MaskedTextChangedListener } from 'ts-input-mask';
+// const email = document.getElementById('email');
+
+// const MaskInput = new PlainMaskInput(document.querySelector('.js-input'), {
+//   mask: '0000-0000-0000-0000',
+// });
+
+// const listener: MaskedTextChangedListener = MaskedTextChangedListener.installOn(' +([375]) [00]-[000]-[00]-[00]', email)
+// const listener: MaskedTextChangedListener = MaskedTextChangedListener.installOn(
+//   primaryFormat,
+//   email,
+//   new class implements MaskedTextChangedListener.ValueListener {
+//       public onTextChanged( \\ method called when value changes
+//           maskFilled: boolean, \\ true when mask is filled
+//           extractedValue: string, \\ text without placeholder: 1234567890
+//           formattedText: string  \\ text with placeholder +7 (123) 456-78-90
+//       ): void {
+//           console.log(maskFilled, extractedValue, formattedText);
+//       }
+//   }(),
+//   affineFormats,
+//   customNotations,
+//   affinityCalculationStrategy,
+//   autocomplete
+// );
